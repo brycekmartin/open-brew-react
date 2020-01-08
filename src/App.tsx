c68ed3b3060501befa103656b404132cd77f7cd4
Bryce Martin <brycekmartin@gmail.com>
2020-01-08 06:21:23 -0500

    updated link text as a test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I'm Learning React!
         </a>
       </header>
     </div>
